17650a6b270bf44aae2a842334de321e1f06b8bd
Edoumou <devforsay@gmail.com>
2021-10-23 19:38:11 +0200

    adds rinkeby test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('./client/node_modules/@truffle/hdwallet-provider');
+require('./client/node_modules/dotenv').config();
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
@@ -7,6 +9,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/v3/${RINKEBY_KEY}`),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )      
     }
   },
   compilers: {
